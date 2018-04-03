991ed78dca9468439d2ab060cd1aaf4a5d459bdc
Jason Spiller <jason@thetargetlab.com>
2018-04-03 17:48:33 -0600

    Adding starter code.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+function convertCurrency(event) {
+  if ('type' in event.target && event.target.type === 'button') {
+    console.log('handle currency conversion here');
+  }
+}
+
+document.body.addEventListener('click', convertCurrency);
