29e0c0112274f9db4e80bfbd773ee4fde4494a92
Jason Spiller <jason@thetargetlab.com>
2018-04-04 02:12:17 -0600

    Made it responsive with Bootstrap.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,12 @@ $(function() {
 	// make the api request
 	$.get('http://data.fixer.io/api/latest?access_key=3e95437b3ad0e0208372ddfeed6587c3', function(response) {
 
-		// convert from base currency (eur) to dollars (usd)
-		function convertBase(intUSD) {
-			return intUSD / response.rates.USD;
+		// convert input to base currency (eur)
+		function convertBase(intInput) {
+			return intInput / response.rates.USD;
 		}
 
+		// convert usd amount entered to appropriate currency
 		function convertCurrency() {
 
 			var intUSD = convertBase($('input[type=text]').val()),
@@ -17,12 +18,30 @@ $(function() {
 			// assign output value rounded conversion
 			intOutputAmount = Math.round(intUSD * response['rates'][strOutputCurrency]);
 
-			$('#output_amount').html(intOutputAmount);
-			$('#output_currency').html(strOutputCurrency);
+			// add the value to the output element
+			if (intOutputAmount < 0 || isNaN(intOutputAmount)) {
+				$('.output').html('Please Enter a Valid Number');
+			} else {
+				$('.output').html(intOutputAmount + ' ' + strOutputCurrency);
+			}
+
+
+			// on each button press remove value, hide output element, update value and then show output element
+			$('.outputValue').on('hide.bs.collapse', function () {
+				$('.output').html('');
+				$('.outputValue').on('hidden.bs.collapse', function () {
+					if (intOutputAmount < 0 || isNaN(intOutputAmount)) {
+						$('.output').html('Please Enter a Valid Number');
+					} else {
+						$('.output').html(intOutputAmount + ' ' + strOutputCurrency);
+					}
+					$('.outputValue').collapse('toggle');
+				})
+			})
 		}
 
 		// add event listener
-		$('input[type="button"]').on('click', convertCurrency);
+		$('button').on('click', convertCurrency);
 
 	});
 })
