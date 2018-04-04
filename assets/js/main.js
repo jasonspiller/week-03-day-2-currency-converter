2026b0e9762677c388dc6ba12c192a3688de682c
Jason Spiller <jason@thetargetlab.com>
2018-04-03 19:28:08 -0600

    It's fully functional. Now time for styling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,28 @@
+$(function() {
 
+	// make the api request
+	$.get('http://data.fixer.io/api/latest?access_key=3e95437b3ad0e0208372ddfeed6587c3', function(response) {
 
-function convertCurrency(event) {
-  if ('type' in event.target && event.target.type === 'button') {
-    console.log('handle currency conversion here');
-  }
-}
+		// convert from base currency (eur) to dollars (usd)
+		function convertBase(intUSD) {
+			return intUSD / response.rates.USD;
+		}
 
-document.body.addEventListener('click', convertCurrency);
+		function convertCurrency() {
+
+			var intUSD = convertBase($('input[type=text]').val()),
+					strOutputCurrency = $(this).val(),
+					intOutputAmount = 0;
+
+			// assign output value rounded conversion
+			intOutputAmount = Math.round(intUSD * response['rates'][strOutputCurrency]);
+
+			$('#output_amount').html(intOutputAmount);
+			$('#output_currency').html(strOutputCurrency);
+		}
+
+		// add event listener
+		$('input[type="button"]').on('click', convertCurrency);
+
+	});
+})
